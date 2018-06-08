c3472b1c8a92a73a18525ca4b35d433d43ba4da1
themaanas <themaanas@gmail.com>
2018-06-08 12:28:31 -0400

    bob

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,15 @@
 package application;
 	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,13 +19,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/laserDefense.fxml"));
-
-        primaryStage.setScene(new Scene(root));
+//		LaserGame gameLasah = new LaserGame("hi");
+//	    gameLasah.startGame();
+		Rectangle rectangle = new Rectangle(100,100,Color.RED);
+        Pane root = FXMLLoader.load(getClass().getResource("/laserDefense.fxml"));
+        Pane newPane = new Pane(root, rectangle);
+//        root.getChildren().addAll(rectangle);
+        primaryStage.setScene(new Scene(newPane));
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
+       
     }
 }
